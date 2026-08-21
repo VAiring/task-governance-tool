e2b33765d30587df21ee675a94fd190c8de973f3
Omoronine <master@omoronine.com>
2026-08-22 03:49:50 +0900

    Remove inactive runner plan test lane

--- tools/test_lanes.py
+++ tools/test_lanes.py
@@ -70,7 +70,6 @@ LANE_MODULES: dict[str, tuple[str, ...]] = {
         "test_m242_evidence_compatibility",
         "test_m242_runner_git",
         "test_m242_runner_model",
-        "test_m242_runner_plan",
         "test_m242_runner_runtime",
         "test_project_scope",
         "test_relocation",
